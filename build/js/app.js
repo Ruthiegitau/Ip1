4e7cd263852d39be2ec231d9ec1cfe28c6a7a435
Patrick Kahoto <Pato@Patricks-MacBook-Pro.local>
2017-01-21 20:53:51 +0300

    final modification

--- build/js/app.js
+++ build/js/app.js
@@ -1,5 +1,5 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-exports.apiKey="9cbe9b3e76518295ba3fde8eb04241e2";
+exports.apiKey="28817fb73405bfa9287c3827cc18005e17f395b1";
 
 },{}],2:[function(require,module,exports){
 var apiKey = require('./../.env').apiKey;
